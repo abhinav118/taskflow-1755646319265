fed0b333a628c6cc403200572b34a84af6a27b57
Abhi <abhinav118@gmail.com>
2025-08-19 19:32:06 -0400

    Add components/TaskCard.tsx

---
+++ components/TaskCard.tsx
@@ -0,0 +1,12 @@
+interface TaskCardProps {
+  hoverable?: any
+  checkable?: any
+}
+
+export default function TaskCard({ hoverable, checkable }: TaskCardProps) {
+  return (
+    <div className="">
+      {/* TaskCard component content */}
+    </div>
+  )
+}
